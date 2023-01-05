b1006ac19fc7c6230ff4aa06b6dd334cd84d90ef
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-01-05 23:17:51 +0530

    Mountain peak array code added

---
+++ Mountain_array.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Mountain_array {
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end){
+            int mid = start + (end -start)/2;
+            if (arr[mid] < arr[mid+1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        return end;
+    }
+    public static void main(String[] args) {
+        int [] arr = {0,2,5,6,8,10,1232325,12456,9,8,7,4,1,-1};
+        int ans = peakIndexInMountainArray(arr);
+        System.out.println(ans);
+    }
+}
