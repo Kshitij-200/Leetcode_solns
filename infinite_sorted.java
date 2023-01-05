c81273dbcc2777b149ebb730fc2e4f2c08e4baf2
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-01-05 23:46:09 +0530

    Infinite sorted array code added

---
+++ infinite_sorted.java
@@ -0,0 +1,10 @@
+package com.leet_solns;
+
+public class infinite_sorted {
+    public static int infinite_array(int [] nums, int target){
+return 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
